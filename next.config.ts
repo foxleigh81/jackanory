7a9e5a2e0eefde28c74d571470d1bc1cb644c9bf
Alex Foxleigh <alex@alexfoxleigh.com>
2025-07-04 23:25:29 +0100

    Relents to the americans and renames all references to 'colour' to 'color'

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
       @use '@/theme/vars' as *;
       @use '@/theme/breakpoints' as bp;
       @use '@/theme/typography' as type;
-      @use '@/theme/colours' as col;
+      @use '@/theme/colors' as col;
       @use '@/theme/utilities' as util;
       @use '@/theme/animations' as animate;
     `
